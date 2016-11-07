cda31e702f238b9154a525a38bd65cdad2f295bf
landhb <landherrb@gmail.com>
2016-11-06 19:58:02 -0500

    Added ErrorHandling and Process helper tools

---
+++ tools/process.c
@@ -0,0 +1,40 @@
+#include <windows.h>
+#include <stdio.h>
+#include <tlhelp32.h>
+#include <tchar.h>
+
+DWORD FindProcessId(const char *processname)
+{
+    HANDLE hProcessSnap;
+    PROCESSENTRY32 pe32;
+    DWORD result = NULL;
+
+    // Take a snapshot of all processes in the system.
+    hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    if (INVALID_HANDLE_VALUE == hProcessSnap) return(FALSE);
+
+    pe32.dwSize = sizeof(PROCESSENTRY32); // <----- IMPORTANT
+
+    // Retrieve information about the first process,
+    // and exit if unsuccessful
+    if (!Process32First(hProcessSnap, &pe32))
+    {
+        CloseHandle(hProcessSnap);          // clean the snapshot object
+        printf("!!! Failed to gather information on system processes! \n");
+        return (DWORD)NULL;
+    }
+
+    do
+    {
+        //printf("Checking process %ls\n", pe32.szExeFile);
+        if (0 == strcmp(processname, pe32.szExeFile))
+        {
+            result = pe32.th32ProcessID;
+            break;
+        }
+    } while (Process32Next(hProcessSnap, &pe32));
+
+    CloseHandle(hProcessSnap);
+
+    return result;
+}
