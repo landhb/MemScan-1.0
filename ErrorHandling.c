fb0747bb3de0f1e809f8b920d025aa3fe5867e9e
landhb-10 <landherrb@gmail.com>
2016-11-06 19:09:49 -0500

    Added Error handling helper function GetLastErrorAsString

---
+++ ErrorHandling.c
@@ -0,0 +1,23 @@
+#include "memblock.h"
+
+
+//Returns the last Win32 error, in string format. Returns an empty string if there is no error.
+const char * GetLastErrorAsString()
+{
+    //Get the error message, if any.
+    DWORD errorMessageID = GetLastError();
+    if(errorMessageID == 0) {
+        return NULL; //No error message has been recorded
+    }
+
+    char * messageBuffer = NULL;
+    size_t size = FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+                                 NULL, errorMessageID, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPSTR)&messageBuffer, 0, NULL);
+
+    //std::string message(messageBuffer, size);
+
+    //Free the buffer.
+    //LocalFree(messageBuffer);
+
+    return messageBuffer;
+}
